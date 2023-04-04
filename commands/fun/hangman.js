69ea6529866d2b781825de659b842381fb222ef6
Jacob Tamez <82856514+TamezJacob@users.noreply.github.com>
2023-04-04 14:07:26 -0500

    Update hangman.js

--- commands/fun/hangman.js
+++ commands/fun/hangman.js
@@ -3,98 +3,91 @@ module.exports = {
     category: "info",
     permissions: [],
     devOnly: false,
-    run: async ({client, message, args}) => {
+    run: async ({ client, message, args }) => {
         const countries = [
-            "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Argentina", "Armenia",
-            "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh",  "Barbados",
-            "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia", "Botswana",
-            "Brazil", "Brunei", "Bulgaria", "Burkina", "Burundi", "Cambodia", "Cameroon", "Canada",
-            "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Croatia", "Cuba", "Cyprus",
-            "Denmark", "Djibouti", "Ecuador", "Egypt", "Eritrea", "Estonia", "Ethiopia", "Fiji", 
-            "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada",
-            "Guatemala", "Guinea", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia",
-            "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory",  "Jamaica", "Japan", "Jordan", "Kazakhstan",
-            "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia",
-            "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia",
-            "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", 
-            "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands",
-            "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Paraguay", "Peru",
-            "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Samoa", "Senegal", "Serbia",
-            "Seychelles", "Singapore", "Slovakia", "Slovenia", "Somalia", "Spain", "Sudan", "Suriname", "Swaziland",
-            "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Tunisia",
-            "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela",
-            "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+            "afghanistan", "albania", "algeria", "andorra", "angola", "argentina", "armenia",
+            "australia", "austria", "azerbaijan", "bahamas", "bahrain", "bangladesh", "barbados",
+            "belarus", "belgium", "belize", "benin", "bhutan", "bolivia", "bosnia", "botswana",
+            "brazil", "brunei", "bulgaria", "burkina", "burundi", "cambodia", "cameroon", "canada",
+            "chad", "chile", "china", "colombia", "comoros", "congo", "croatia", "cuba", "cyprus",
+            "denmark", "ejibouti", "ecuador", "egypt", "eritrea", "estonia", "ethiopia", "fiji",
+            "finland", "france", "gabon", "gambia", "georgia", "germany", "ghana", "greece", "grenada",
+            "guatemala", "guinea", "guyana", "haiti", "honduras", "hungary", "iceland", "india", "indonesia",
+            "iran", "iraq", "ireland", "israel", "italy", "ivory", "jamaica", "japan", "jordan", "kazakhstan",
+            "kenya", "kiribati", "kosovo", "kuwait", "kyrgyzstan", "laos", "latvia", "lebanon", "lesotho", "liberia",
+            "libya", "liechtenstein", "lithuania", "luxembourg", "macedonia", "madagascar", "malawi", "malaysia",
+            "maldives", "mali", "malta", "mauritania", "mauritius", "mexico", "micronesia", "moldova", "monaco",
+            "mongolia", "montenegro", "morocco", "mozambique", "myanmar", "namibia", "nauru", "nepal", "netherlands",
+            "nicaragua", "niger", "nigeria", "norway", "oman", "pakistan", "palau", "panama", "paraguay", "peru",
+            "philippines", "poland", "portugal", "qatar", "romania", "russia", "rwanda", "samoa", "senegal", "serbia",
+            "seychelles", "singapore", "slovakia", "slovenia", "somalia", "spain", "sudan", "suriname", "swaziland",
+            "sweden", "switzerland", "syria", "taiwan", "tajikistan", "tanzania", "thailand", "togo", "tonga", "tunisia",
+            "turkey", "turkmenistan", "tuvalu", "uganda", "ukraine", "uruguay", "uzbekistan", "vanuatu", "venezuela",
+            "vietnam", "yemen", "zambia", "zimbabwe"
         ];
 
         random = (Math.floor(Math.random() * 512) % countries.length);
-        word = countries[random]
+        word = countries[random];
         wrongs = 0;
         const guessed = [];
         const lettersOfWord = Array.from(word);
+        const wrongCount = [];
 
         console.log(word);
-        for(let i = 0; i > lettersOfWord.length; i++){
-            console.log(lettersOfWord[i]);
-            console.log(countries[i]);
-        }
-        printDisplay(word, wrongs, guessed, message);
+        printDisplay(word, wrongs, guessed, message)
 
         const filter = message => message.author.id === message.author.id;
         let options = {
-            max: 5,
-            time: 1500
+            max: 26,
+            time: 150000
         };
-        let collector = message.channel.createMessageCollector(filter, options);
-        
+        let collector = message.channel.createMessageCollector(options, filter);
+
         collector.on('collect', (message) => {
-            //console.log(`Collected ${message.content}`);
-            game(lettersOfWord, wrongs, guessed, message, message.content);
+            let num = game(lettersOfWord, wrongs, guessed, message, message.content, wrongCount);
+            if (num > 4) collector.stop();
         });
-    
+
         collector.on('end', (collected) => {
             console.log(`Collected ${collected.size} items`);
+            message.reply('Game Over');
         });
 
     }
 }
 
-async function game(lettersOfWord, wrongs, guessed, message, letterGuessed){
+function game(lettersOfWord, wrongs, guessed, message, letterGuessed, wrongCount) {
 
-    if (letterGuessed.includes('`') || letterGuessed.includes('|') || letterGuessed.includes('‾') || letterGuessed.includes('_')){ return; }
-    console.log(letterGuessed);
+    if (letterGuessed.includes('`') || letterGuessed.includes('|') || letterGuessed.includes('‾') || letterGuessed.includes('_')) { return; }
     guessed.push(letterGuessed);
     found = false;
-    for(let i = 0; i < lettersOfWord.length; i++){
-        if(letterGuessed == lettersOfWord[i]){
+    for (let i = 0; i < lettersOfWord.length; i++) {
+        if (letterGuessed == lettersOfWord[i]) {
             found = true;
-            console.log('POG');
             break;
         }
     }
-    if(!found) { wrongs += guessed.length; }
+    if (!found) { wrongCount.push(letterGuessed); wrongs += wrongCount.length; }
     else { found = false; }
     printDisplay(word, wrongs, guessed, message)
+    return wrongs;
+
 }
 
-async function printDisplay(word, wrongs, guessed, message){
-    console.log(wrongs);
+async function printDisplay(word, wrongs, guessed, message) {
     message.reply('```'
-    + '|‾‾‾‾‾‾|   \n|     '
-    + (wrongs > 0 ? '🎩' : ' ')
-    + '   \n|     '
-    + (wrongs > 1 ? '😟' : ' ')
-    + '   \n|     '
-    + (wrongs > 2 ? '👕' : ' ')
-    + '   \n|     '
-    + (wrongs > 3 ? '🩳' : ' ')
-    + '   \n|    '
-   + (wrongs > 4 ? '👞👞' : ' ')
-    + '   \n|     \n|__________\n\n'
-    + word.split('').map(l => guessed.includes(l) ? l : '_').join(' ')
-    + '```');
+        + '|‾‾‾‾‾‾|   \n|     '
+        + (wrongs > 0 ? '🎩' : ' ')
+        + '   \n|     '
+        + (wrongs > 1 ? '😟' : ' ')
+        + '   \n|     '
+        + (wrongs > 2 ? '👕' : ' ')
+        + '   \n|     '
+        + (wrongs > 3 ? '🩳' : ' ')
+        + '   \n|    '
+        + (wrongs > 4 ? '👞👞' : ' ')
+        + '   \n|     \n|__________\n\n'
+        + word.split('').map(l => guessed.includes(l) ? l : '_').join(' ')
+        + '```');
 
-    if(wrongs > 4){
-        message.reply("Game Over");
-        throw new Error();
-    }
 }
